cb62ca258e24108ddf51233c11106518f0266372
Manan Mukim <50824004+mukimmanan@users.noreply.github.com>
2025-12-05 15:53:58 +0530

    Removed INFO Logs

---
+++ spark/notebooks/hive_testing.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .master("spark://spark-master:7077") \
+    .appName("HiveTesting") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+
+# spark.sql("DROP TABLE IF EXISTS stocks.testing_hive_table3")
+
+spark.sql("SHOW DATABASES").show()
+spark.sql("USE stocks")
+
+data = spark.sql("SHOW DATAASES")
+
+
+# data.write.mode("overwrite").saveAsTable("stocks.testing_parquet")
+# data.write.mode("overwrite").format("csv").option("header", "true").saveAsTable("stocks.testing_csv")
+data.write.mode("overwrite").option("truncate", "true").format("delta").saveAsTable("stocks.delta")
+
+# spark.sql("SELECT * FROM stocks.testing_parquet").show()
+# spark.sql("SELECT * FROM stocks.testing_csv").show()
+spark.sql("SELECT * FROM stocks.delta").show()
